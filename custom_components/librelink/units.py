d4080dbccdcbe85eec0edb1d8aff7518162f3a2e
Wellington Marthas <w@dyntime.com>
2024-08-20 22:27:02 -0300

    Some customizations

---
+++ custom_components/librelink/units.py
@@ -0,0 +1,19 @@
+"""Units of measurement for LibreLink integration."""
+from collections.abc import Callable
+from dataclasses import dataclass
+
+@dataclass
+class UnitOfMeasurement:
+    """Unit of measurement for LibreLink integration."""
+
+    unit_of_measurement: str
+    suggested_display_precision: int
+    from_mg_per_dl: Callable[[float], float]
+
+UNITS_OF_MEASUREMENT = (
+    UnitOfMeasurement(
+        unit_of_measurement="mg/dL",
+        suggested_display_precision=0,
+        from_mg_per_dl=lambda x: x,
+    ),
+)
